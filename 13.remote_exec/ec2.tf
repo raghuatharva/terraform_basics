552f793518b5e1b53ac66a324ae78dc7b8550d80
raghuatharva <raghuatharva95@gmail.com>
2025-01-23 08:19:51 +0530

    cc

---
+++ 13.remote_exec/ec2.tf
@@ -0,0 +1,23 @@
+resource "null_resource" "xy" { # we are not using any server here, my pc is connecting directly to that through ssh 
+                               # and executing the commands.
+  provisioner "remote-exec" {
+ inline = [
+    "sudo yum install httpd -y",
+    "sudo systemctl start httpd",
+    "sudo systemctl enable httpd",
+    "sudo echo '<h1>Deployed via Terraform remote exec</h1>' > /var/www/html/index.html"
+  ]
+  }
+
+  connection {
+    type        = "ssh"
+    user  = "ec2-user"
+    password = "DevOps321"
+    host        = "98.81.240.42"
+  
+}
+}
+
+
+## we dont need provider.tf for this remote exec as
+# we are not creating any resources here
